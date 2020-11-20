ea6ba08433e79a31cb407a3715b5507011fb7db2
mark.lin <mark@maicoin.com>
2020-11-20 14:02:06 +0800

    *: implement v1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/markya0616/mod-test
+
+go 1.15
+
+require google.golang.org/grpc v1.33.2
---
+++ utils/utils.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"fmt"
+
+	_ "google.golang.org/grpc"
+)
+
+func PrintHello() {
+	fmt.Println("Hello world!!")
+}
